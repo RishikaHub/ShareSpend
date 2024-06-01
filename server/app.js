bc2c4a5706d24ca87a9958b60834a5ad7b8996cf
RishikaHub <rishika.bussa31@gmail.com>
2024-06-01 08:20:33 +0530

    Minor changes - v1.0.1

--- server/app.js
+++ server/app.js
@@ -21,10 +21,9 @@ app.use(expensesRouter);
 app.use(debtsRouter);
 
 // !IMPORTANT: Create .env file with password
-const password = process.env.PASSWORD || "changePasswordHere";
-// let devUrl = `mongodb+srv://admin:${password}@fairsplit.fjvgxmg.mongodb.net/?retryWrites=true&w=majority`;
+const password = process.env.PASSWORD ;
 var mongoDB = process.env.MONGODB_URI;
-// Set up the Mongoose connection.
+
 mongoose
   .connect(mongoDB, {
     useNewUrlParser: true,
@@ -33,15 +32,12 @@ mongoose
   .catch((error) => handleError(error));
 let db = mongoose.connection;
 
-// Bind the connection to an error event to get notification of connection
-// errors.
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
-// Displays a success message when the connection is successfully made.
+
 db.once("open", function () {
   console.log("Connected successfully.");
 });
 
-// Set up the port to listen on.
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}.`);
